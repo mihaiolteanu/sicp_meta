d0bf30c1784a0891f02328a95fa68c255041cda0
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2018-07-17 12:20:11 +0300

    Add the driver loop

--- evaluator.import.scm
+++ evaluator.import.scm
@@ -350,6 +350,37 @@
            (procedure-environment procedure))))
         (else (error ("Unknown procedure type: MY-APPLY" procedure)))))
 
+(define input-prompt "> ")
+(define output-prompt "> ")
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output
+           (my-eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-lopp))
+
+(define (prompt-for-input string)
+  (newline) (newline)
+  (display string) (newline))
+
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display
+       (list 'compound-procedure
+             (procedure-parameters object)
+             (procedure-body object)
+             '<procedure-env>))
+      (display object)))
+
+
+)
+
 
 )
 
