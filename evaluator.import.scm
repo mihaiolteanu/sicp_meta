dfad4c7b13432c19d66a409037c6a2ec83c9429e
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2018-07-19 09:42:21 +0300

    Reallign stuff

--- evaluator.import.scm
+++ evaluator.import.scm
@@ -315,51 +315,52 @@
 (define (my-eval exp env)
   (cond
    ((self-evaluation? exp) exp)
-        ((variable? exp)
-         (lookup-variable-value exp env))
-        ((quoted? exp)
-         (text-of-quotation exp))
-        ((assignment? exp)
-         (eval-assignment exp env))
-        ((definition? exp)
-         (eval-definition exp env))
-        ((if? exp)
-         (eval-if exp env))
-        ((or? exp)
-         (eval-or exp env))
-        ((and? exp)
-         (eval-and exp env))
-        ((lambda? exp)
-         (make-procedure
-          (lambda-parameters exp)
-          (lambda-body exp)
-          env))
-        ((begin? exp)
-         (eval-sequence
-          (begin-actions exp)
-          env))
-        ((cond? exp)
-         (my-eval (cond->if exp) env))
-        ((application? exp)
-         (my-apply (my-eval (operator exp) env)
-                   (list-of-values
-                    (operands exp)
-                    env)))
-        (else (error "Unknown expression type: MY-EVAL" exp))))
+   ((variable? exp)
+    (lookup-variable-value exp env))
+   ((quoted? exp)
+    (text-of-quotation exp))
+   ((assignment? exp)
+    (eval-assignment exp env))
+   ((definition? exp)
+    (eval-definition exp env))
+   ((if? exp)
+    (eval-if exp env))
+   ((or? exp)
+    (eval-or exp env))
+   ((and? exp)
+    (eval-and exp env))
+   ((lambda? exp)
+    (make-procedure
+     (lambda-parameters exp)
+     (lambda-body exp)
+     env))
+   ((begin? exp)
+    (eval-sequence
+     (begin-actions exp)
+     env))
+   ((cond? exp)
+    (my-eval (cond->if exp) env))
+   ((application? exp)
+    (my-apply (my-eval (operator exp) env)
+              (list-of-values
+               (operands exp)
+               env)))
+   (else (error "Unknown expression type: MY-EVAL" exp))))
 
 (define (my-apply procedure arguments)
-  (cond ((primitive-procedure? procedure)
-         (apply-primitive-procedure
-          procedure
-          arguments))
-        ((compound-procedure? procedure)
-         (eval-sequence
-          (procedure-body procedure)
-          (extend-environment
-           (procedure-parameters procedure)
-           arguments
-           (procedure-environment procedure))))
-        (else (error ("Unknown procedure type: MY-APPLY" procedure)))))
+  (cond
+   ((primitive-procedure? procedure)
+    (apply-primitive-procedure
+     procedure
+     arguments))
+   ((compound-procedure? procedure)
+    (eval-sequence
+     (procedure-body procedure)
+     (extend-environment
+      (procedure-parameters procedure)
+      arguments
+      (procedure-environment procedure))))
+   (else (error ("Unknown procedure type: MY-APPLY" procedure)))))
 
 (define input-prompt "> ")
 (define output-prompt "")
